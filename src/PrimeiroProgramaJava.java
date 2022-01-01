5fd200ecff760bb4b836cec10e0127b5518d8bda
JoaquimRaphael <guinho.ribeiro_cg@hotmail.com>
2021-12-31 23:52:08 -0300

    primeiro commit via intelj

---
+++ src/PrimeiroProgramaJava.java
@@ -0,0 +1,13 @@
+import Model.Gato;
+
+public class PrimeiroProgramaJava {
+    public static void main(String[] args) {
+       /* int a = 5;
+        int b = 3;
+        System.out.println("Hello Word " + (a+ b));*/
+
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+    }
+}
