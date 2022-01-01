7fcfefb56fee98120fcc33b1bed3fa8a68c9e0ee
JoaquimRaphael <guinho.ribeiro_cg@hotmail.com>
2022-01-01 00:11:46 -0300

    criando classe Livro

--- src/PrimeiroProgramaJava.java
+++ src/PrimeiroProgramaJava.java
@@ -10,4 +10,39 @@ public class PrimeiroProgramaJava {
         System.out.println(gato);
 
     }
+
+
 }
+  class Livro {
+    private String nome;
+    private Integer numeroPaginas;
+
+      public Livro(String nome, Integer numeroPaginas) {
+          this.nome = nome;
+          this.numeroPaginas = numeroPaginas;
+      }
+
+      public String getNome() {
+          return nome;
+      }
+
+      public void setNome(String nome) {
+          this.nome = nome;
+      }
+
+      public Integer getNumeroPaginas() {
+          return numeroPaginas;
+      }
+
+      public void setNumeroPaginas(Integer numeroPaginas) {
+          this.numeroPaginas = numeroPaginas;
+      }
+
+      @Override
+      public String toString() {
+          return "Livro{" +
+                  "nome='" + nome + '\'' +
+                  ", numeroPaginas=" + numeroPaginas +
+                  '}';
+      }
+  }
